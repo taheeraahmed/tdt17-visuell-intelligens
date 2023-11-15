0fc21954360aab355b4b08c02e8f2e75db697b22
wolneberg <ingrid_marie@wolneberg.no>
2023-11-15 16:51:03 +0100

    start unetliver

---
+++ models/unetLiver.py
@@ -0,0 +1,45 @@
+from fastMONAI.vision_all import *
+
+from sklearn.model_selection import train_test_split
+from monai.apps import DecathlonDataset
+from pandas import DataFrame
+import numpy as np
+import pyfiglet
+
+from monai.losses import DiceCELoss
+from monai.networks.nets import UNet
+
+result = pyfiglet.figlet_format("VI babes", font = "slant"  ) 
+print(result) 
+
+data_path = '/cluster/projects/vc/data/mic/open/MSD'
+
+print('Loading data..')
+training_data = DecathlonDataset(root_dir=data_path, task="Task03_Liver", section="training", download=False, cache_num=0, num_workers=3)
+print('Done loading data!')
+
+df = DataFrame(training_data.data)
+train_df, test_df = train_test_split(df, test_size=0.1, random_state=42)
+
+codes = np.unique(med_img_reader(train_df.label.tolist()[0]))
+n_classes = len(codes)
+
+print('MedData stuff..')
+bs=4
+size=[224,224,128]
+med_dataset = MedDataset(img_list=train_df.label.tolist(), dtype=MedMask, max_workers=12)
+resample, reorder = med_dataset.suggestion()
+item_tfms = [ZNormalization(), PadOrCrop(size), RandomAffine(scales=0, degrees=5, isotropic=True)]
+dblock = MedDataBlock(blocks=(ImageBlock(cls=MedImage), MedMaskBlock), splitter=RandomSplitter(seed=42), get_x=ColReader('image'), get_y=ColReader('label'), item_tfms=item_tfms,reorder=reorder,resample=resample)
+dls = dblock.dataloaders(train_df, bs=bs)
+print('Done with MedData stuff..')
+
+model = UNet(spatial_dims=3, in_channels=1, out_channels=n_classes, channels=(16, 32, 64, 128, 256),strides=(2, 2, 2, 2), num_res_units=2)
+loss_func = CustomLoss(loss_func=DiceCELoss(to_onehot_y=True, include_background=True, softmax=True))
+
+learn = Learner(dls, model, loss_func=loss_func, opt_func=ranger, metrics=multi_dice_score)#.to_fp16()
+learn.lr_find()
+
+
+
+
