f12c8a4e8db21a543e9f5be25626fabbc66b4477
Taheera Ahmed <taheera@hotmail.com>
2023-11-21 10:17:32 +0100

    Adding carbontracker

--- train.py
+++ train.py
@@ -1,14 +1,16 @@
 from helpers.set_up import set_up
 from models.unetSpleen import unet_spleen
+from codecarbon import track_emissions
+
 import sys
 
+@track_emissions()
 def main():
     logger, project_root = set_up()
     sys.path.append(project_root)
     
     unet_spleen(logger=logger)
     logger.info('Finished running the code')
-
     
 if __name__ == "__main__":
-    main()  # Call the main function if this script is executed as the main program
+    main() 
