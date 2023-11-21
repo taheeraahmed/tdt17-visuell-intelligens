6f1530f955b420e34281d350b0839e7f46e66e7e
taheeraahmed <taheera@hotmail.com>
2023-11-21 18:03:25 +0100

    Add directory creation for logs

--- helpers/set_up.py
+++ helpers/set_up.py
@@ -2,12 +2,14 @@ import pyfiglet
 import logging
 import os
 from datetime import datetime
+from helpers.create_dir import create_directory_if_not_exists
 
 def set_up():
     result = pyfiglet.figlet_format("VI babes", font = "slant") 
     print(result) 
     project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-
+    
+    create_directory_if_not_exists("logs")
     now = datetime.now()
     dt_string = now.strftime("%d-%m-%Y-%H-%M-%S")
     LOG_FILE = f"logs/{dt_string}.txt"
