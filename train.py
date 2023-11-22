edbf8c777fbe596663ad91ed202a3f1d372d9dde
Taheera Ahmed <taheera@hotmail.com>
2023-11-22 14:58:22 +0100

    Add name of project

--- train.py
+++ train.py
@@ -20,7 +20,7 @@ def main(args):
     
     if check_nvml_error(logger=logger) == 0:
         logger.info('Code carbon is working B)')
-        with EmissionsTracker(project_name=args.model) as tracker:
+        with EmissionsTracker(project_name=model, log_level="error") as tracker:
             run_models(model, logger, job_id=job_id)
     else: 
         logger.warning('Dropped carbon tracker :/')
