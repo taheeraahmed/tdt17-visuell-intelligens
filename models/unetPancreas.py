e89c161fd86e5545b625a538a8c9c594a97cab71
HeleneAmlie <helene@amlie.org>
2023-11-22 19:27:48 +0100

    unet pancreas

--- models/unetPancreas.py
+++ models/unetPancreas.py
@@ -2,13 +2,16 @@ from fastMONAI.vision_all import *
 
 from sklearn.model_selection import train_test_split
 from monai.apps import DecathlonDataset
-from monai.transforms import RandAffine
+from monai.transforms import RandAffined
 from pandas import DataFrame
 import numpy as np
 import pyfiglet
 
 from monai.losses import DiceCELoss
-from monai.networks.nets import UNet
+from monai.networks.nets import (
+    UNet,
+    UNETR
+)
 
 def unet_pancreas(logger):
     pd.set_option('display.max_columns', None)
@@ -25,12 +28,12 @@ def unet_pancreas(logger):
     df = DataFrame(training_data.data)
     train_df, test_df = train_test_split(df, test_size=0.1, random_state=42)
     logger.info('Analysing training data and test data')
-    logger.info('Shape of train df: ',train_df.shape)
-    logger.info('Shape of test df: ', test_df.shape)
+    print('Shape of train df: ',train_df.shape)
+    print('Shape of test df: ', test_df.shape)
 
     med_dataset = MedDataset(img_list=train_df.label.tolist(), dtype=MedMask, max_workers=12)
     logger.info('Information of one training image')
-    logger.info(med_dataset.df.head())
+    print(med_dataset.df.head())
 
     codes = np.unique(med_img_reader(train_df.label.tolist()[0]))
     n_classes = len(codes)
@@ -40,30 +43,43 @@ def unet_pancreas(logger):
     size=[224,224,128] # make every image this size
     med_dataset = MedDataset(img_list=train_df.label.tolist(), dtype=MedMask, max_workers=12)
     summary_df = med_dataset.summary()
-    logger.info(summary_df.head())
+    print(summary_df.head())
     resample, reorder = med_dataset.suggestion() # this returns Voxel value that appears most often in dim_0, dim_1 and dim_2, and whether the data should be reoriented
-    logger.info("Voxel value for dim_0, dim_1 and dim_2: ", resample, "Shoud reorient: ", reorder)
+    print("Voxel value for dim_0, dim_1 and dim_2: ", resample)
+    print("Should reorder data: ", reorder)
+
     
-    item_tfms = [PadOrCrop(size), RandomAffine()] #Data augmentation
+    item_tfms = [ZNormalization(), PadOrCrop(size)] #Data augmentation
+    # item_tfms = [ZNormalization(), PadOrCrop(size), RandomAffine(degrees=15, translation=15) ] #Data augmentation
+    # item_tfms = [ZNormalization(), PadOrCrop(size), RandomGamma()] #Data augmentation
+    # item_tfms = [ZNormalization(), PadOrCrop(size), RandomNoise()] #Data augmentation
+
+    # item_tfms = [ZNormalization(), PadOrCrop(size), RandAffined(keys=['image', 'label'], mode=('bilinear', 'nearest'), shear_range=(0.5, 0.5), translate_range=(15,15))] #Data augmentation
 
     # item_tfms = [ZNormalization(), PadOrCrop(size), RandomAffine(scales=0, degrees=5, isotropic=True)] #Data augmentation
     dblock = MedDataBlock(blocks=(ImageBlock(cls=MedImage), MedMaskBlock), splitter=RandomSplitter(seed=42), get_x=ColReader('image'), get_y=ColReader('label'),reorder=reorder,resample=resample, item_tfms=item_tfms) # item_tfms=item_tfms
     dls = dblock.dataloaders(train_df, bs=bs)
-    logger.info('Length of traning data: ',len(dls.train_ds.items), 'Length of validation data: ', len(dls.valid_ds.items))
+    print('Length of traning data: ',len(dls.train_ds.items), 'Length of validation data: ', len(dls.valid_ds.items))
+    dls.show_batch(anatomical_plane=0, slice_index=20)
+    plt.savefig('models/Pancreas/v1/images/Examples_of_data_pancreas_slice.png')
+    plt.close()
     dls.show_batch(anatomical_plane=0)
-    plt.title('Image/label')
-    plt.savefig('Pancreas/v1/images/Examples_of_data_pancreas.png')
+    plt.savefig('models/Pancreas/v1/images/Examples_of_data_pancreas.png')
+    plt.close()
+    dls.show_batch(anatomical_plane=1)
+    plt.savefig('models/Pancreas/v1/images/Examples_of_data_pancreas_ap1.png')
     plt.close()
     logger.info('Done with cache and data augmentation..')
 
-    model = UNet(spatial_dims=3, in_channels=1, out_channels=n_classes, channels=(16, 32, 64, 128, 256),strides=(2, 2, 2, 2), num_res_units=2)
+    model = v1(spatial_dims=3, in_channels=1, out_channels=n_classes, img_size=size)
+    # model = UNet(spatial_dims=3, in_channels=1, out_channels=n_classes, channels=(16, 32, 64, 128, 256),strides=(2, 2, 2, 2), num_res_units=2)
     loss_func = CustomLoss(loss_func=DiceCELoss(to_onehot_y=True, include_background=True, softmax=True))
 
     logger.info('Learning rate')
     learn = Learner(dls, model, loss_func=loss_func, opt_func=ranger, metrics=multi_dice_score)#.to_fp16()
     lr = learn.lr_find() #set learning rate?
-    logger.info('learning rate', lr)
-    plt.savefig('Pancreas/v1/images/learning.png')
+    print('learning rate', lr)
+    plt.savefig('models/Pancreas/v1/images/learning.png')
     plt.close()
     epochs=50
     logger.info('Training')
@@ -72,25 +88,26 @@ def unet_pancreas(logger):
 
     logger.info('Results from training')
     learn.show_results(anatomical_plane=0, ds_idx=1)
-    plt.savefig('Pancreas/v1/images/result_training_pancreas.png')
+    plt.savefig('models/Pancreas/v1/images/result_training_pancreas.png')
     plt.close()
 
     logger.info('Testing')
     learn.load('Pancreas/v1/models/trained.Pancreas-model')
     test_dl = learn.dls.test_dl(test_df[:10],with_labels=True)
     test_dl.show_batch(anatomical_plane=0, figsize=(10,10))
-    plt.savefig('Pancreas/v1/images/result_testing_pancreas.png')
+    plt.savefig('models/Pancreas/v1/images/result_testing_pancreas.png')
     plt.close()
 
 
     logger.info('result from test')
     pred_acts, labels = learn.get_preds(dl=test_dl)
-    logger.info('predicted shape: ', pred_acts.shape, 'Label shape', labels.shape)
-    logger.info('Dice score for label_1 and label 2: ',multi_dice_score(pred_acts, labels))
+    print('predicted shape: ', pred_acts.shape, 'Label shape', labels.shape)
+    print('Dice score for label_1 and label 2: ',multi_dice_score(pred_acts, labels))
     learn.show_results(anatomical_plane=0, dl=test_dl)
-    plt.savefig('Pancreas/v1/images/result_training.png')
+    plt.savefig('models/Pancreas/v1/images/result_training.png')
     plt.close()
 
     logger.info('Export')
     learn.export('Pancreas/v1/models/pancreas_model.pkl')
+    # logger.info('Did not save trained model')
 
