3bf35cd624ff15f4392fba046fb343471822729c
Taheera Ahmed <taheera@hotmail.com>
2023-11-25 12:32:27 +0100

    Refactor rsync command and add SLURM job options

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@
 
 # Generate a unique identifier
 UNIQUE_ID=$(date +%Y%m%d-%H%M%S)
-AUGMENTATION="baseline" # "rand_affine", "rand_noise", "rand_gamma", "baseline"
+AUGMENTATION="baseline"  # "rand_affine", "rand_noise", "rand_gamma", "baseline"
 MODEL="unetr_spleen"     # "unet_spleen", "unet_liver", "unet_pancreas", 'unetr_spleen'
 
 JOB_NAME="${AUGMENTATION}-${MODEL}-${UNIQUE_ID}"
@@ -14,8 +14,30 @@ CODE_PATH="/cluster/home/taheeraa/runs/code/$AUGMENTATION-$MODEL-$UNIQUE_ID"
 # Copy the code with rsync, excluding .venv
 echo "Copying code to $CODE_PATH"
 mkdir -p $CODE_PATH
-rsync -av --exclude='.venv' --exclude='logs' --exclude='models' --exclude='idun' --exclude='emissions.csv' --exclude='notebooks' --exclude='outputs' --exclude='models/__pycache__' --exclude='helpers/__pycache__' --exclude='.git' /cluster/home/taheeraa/code/tdt17-visuell-intelligens/ $CODE_PATH
+
+rsync -av \
+  --exclude='.venv' \
+  --exclude='logs' \
+  --exclude='models' \
+  --exclude='idun' \
+  --exclude='emissions.csv' \
+  --exclude='notebooks' \
+  --exclude='outputs' \
+  --exclude='models/__pycache__' \
+  --exclude='helpers/__pycache__' \
+  --exclude='.git' \
+  /cluster/home/taheeraa/code/tdt17-visuell-intelligens/ $CODE_PATH
 
 # Submit the job to SLURM with the necessary environment variables
 echo "Running slurm job from $CODE_PATH"
-sbatch --partition=GPUQ --account=ie-idi --time=20:00:00 --nodes=1 --ntasks-per-node=1 --mem=50G --gres=gpu:1 --job-name=$JOB_NAME --output=$OUTPUT_FILE --export=UNIQUE_ID=$UNIQUE_ID,AUGMENTATION=$AUGMENTATION,MODEL=$MODEL,CODE_PATH=$CODE_PATH $CODE_PATH/unetSpleen.slurm
+sbatch --partition=GPUQ \
+  --account=ie-idi \
+  --time=20:00:00 \
+  --nodes=1 \
+  --ntasks-per-node=1 \
+  --mem=50G \
+  --gres=gpu:1 \
+  --job-name=$JOB_NAME \
+  --output=$OUTPUT_FILE \
+  --export=UNIQUE_ID=$UNIQUE_ID,AUGMENTATION=$AUGMENTATION,MODEL=$MODEL,CODE_PATH=$CODE_PATH \
+  $CODE_PATH/unetSpleen.slurm
